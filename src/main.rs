e8cc5d151b5e2cfcdb2e84d4ff9a86debaa81279
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-02-05 23:18:22 +0100

    Created a channel for messages transmission

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::net::TcpListener;
+use std::sync::{
+    mpsc::{channel, Receiver, Sender},
+    Arc, Mutex,
+};
 use std::thread::spawn;
-use std::sync::{Mutex, Arc};
 
 fn main() {
-   let listener = TcpListener::bind("0:0:0:0:9090").unwrap(); 
+    let listener = TcpListener::bind("0:0:0:0:9090").unwrap();
+    let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
 }
