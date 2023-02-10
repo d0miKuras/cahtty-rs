b110ccc2fc91468d885fb0c581e860c0294fa531
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-02-10 19:04:04 +0100

    Vec<Senders<String>> is now aliased to Senders

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use std::thread::spawn;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9090").unwrap();
     let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
-    let senders: Vec<Sender<String>> = Vec::new();
-    let senders_mutex: Mutex<Vec<Sender<String>>> = Mutex::new(senders); // since this will be shared between threads
-    let senders_mutex_ptr: Arc<Mutex<Vec<Sender<String>>>> = Arc::new(senders_mutex);
+    type Senders = Vec<Sender<String>>;
+    let senders: Senders = Vec::new();
+    let senders_mutex: Mutex<Senders> = Mutex::new(senders); // since this will be shared between threads
+    let senders_mutex_ptr: Arc<Mutex<Senders>> = Arc::new(senders_mutex);
 }
