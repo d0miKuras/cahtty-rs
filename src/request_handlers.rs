cfad21e3a546a72054d90abf45545f1d1271994b
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-02-12 21:39:12 +0100

    Added some functionality and error handling to message receiver

--- src/request_handlers.rs
+++ src/request_handlers.rs
@@ -7,4 +7,11 @@ pub fn receive_messages(
     receiver: Receiver<String>,
     senders_mutex_pointer: Arc<Mutex<Vec<Sender<String>>>>,
 ) {
+    loop {
+        let message_result = receiver.recv();
+        if let Err(e) = message_result {
+            eprintln!("Error waiting for message result");
+            continue;
+        }
+    }
 }
