10ca7ad8b526c46be7fe9e68e117deb4c19c752d
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-02-17 21:03:56 +0100

    Started implementing sent messages handling

--- src/request_handlers.rs
+++ src/request_handlers.rs
@@ -1,6 +1,10 @@
-use std::sync::{
-    mpsc::{Receiver, Sender},
-    Arc, Mutex,
+use std::{
+    io::{BufRead, BufReader},
+    net::TcpStream,
+    sync::{
+        mpsc::{Receiver, Sender},
+        Arc, Mutex,
+    },
 };
 
 pub fn receive_messages(
@@ -19,5 +23,16 @@ pub fn receive_messages(
         for sender in senders {
             sender.send(msg.to_string()).expect("Error sending message");
         }
+    }
+}
+
+pub fn handle_sent_messages(stream: TcpStream, sender: Sender<String>) {
+    let mut buffer = BufReader::new(stream);
+    let mut message = String::new();
+
+    let request = buffer.read_line(&mut message);
+    if let Err(e) = request {
+        eprintln!("Failed to receive a message: {e}");
+        return;
     }
 }
